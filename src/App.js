ea45aa37738b66fb4d40b135442b05eac4a132f0
kevinkim <shkim2000@gmail.com>
2021-10-23 01:38:47 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-function Food({ name, picture }) {
+
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h3>I like {name}</h3>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
+    rating: 5,
     image:
     "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
     id: 2,
     name: "Samgyeopsal",
+    rating: 4.9,
     image:
     "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
     id: 3,
     name: "Bibimbap",
+    rating: 4.1,
     image:
     "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
     id: 4,
     name: "Doncasu",
+    rating: 3.2,
     image:
     "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
     id: 5,
     name: "Kimbap",
+    rating: 4.8,
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
@@ -46,7 +60,14 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />)}
+      {foodILike.map(dish => (
+        <Food 
+        key={dish.id} 
+        name={dish.name} 
+        picture={dish.image} 
+        rating={dish.rating} 
+        />
+      ))}
     </div>
   )
 }
